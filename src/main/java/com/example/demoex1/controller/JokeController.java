f7d255b01472dd47dc2abb4a18513f7bda6ba876
trung <trung151192@gmail.com>
2023-01-22 23:00:21 +0900

    add controller

---
+++ src/main/java/com/example/demoex1/controller/JokeController.java
@@ -0,0 +1,24 @@
+package com.example.demoex1.controller;
+
+import com.example.demoex1.services.JokeService;
+import com.example.demoex1.services.JokeServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService){
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/demoex1/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.example.demoex1.services;
+
+public interface JokeService {
+    String getJoke();
+}
